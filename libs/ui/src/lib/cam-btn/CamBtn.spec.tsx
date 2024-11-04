88104e8a533ed0fb606ebf6e95adc3df3bcfb30a
Ysrbolles <ysrbolles@gmail.com>
2024-11-04 16:40:29 +0100

    Refactor project configuration and add Jest setup for UI library

--- libs/ui/src/lib/cam-btn/CamBtn.spec.tsx
+++ libs/ui/src/lib/cam-btn/CamBtn.spec.tsx
@@ -1,10 +1,18 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 
 import CamBtn from './CamBtn';
 
 describe('CamBtn', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<CamBtn />);
+    const { baseElement } = render(
+      <CamBtn
+        variant="primary"
+        size="md"
+        children="button"
+        onClick={() => {}}
+      />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
